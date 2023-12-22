4ea5971908359b43fa1eff5170222a851489258d
Vlad <sizyh06@gmail.com>
2023-12-22 18:07:27 +0800

    add progress bar for health

--- code/enemy_1.gd
+++ code/enemy_1.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
+var health	# init in _ready function
+var isDead = false
+@onready var healthBar = $ProgressBar
+
 var speed = 50
 var swoop_speed = 75
 
 var last_direction = Vector2.ZERO
 
-var animated_sprite
+var animated_sprite	# init in _ready function
 
 var direction_change_timer = 0
 var direction_change_interval = 3
@@ -19,10 +23,15 @@ var max_position = Vector2(800, 430)
 
 func _ready():
 	animated_sprite = $AnimatedSprite2D
+	health = healthBar.max_value
 	pick_random_direction()
 	add_to_group("Enemy")
 	
 func _physics_process(delta):
+	
+	update_health()
+	die()
+	
 	if swoop:
 		var direction_to_player = (player.position - position).normalized() 
 		velocity = direction_to_player * swoop_speed
@@ -72,6 +81,18 @@ func pick_random_direction():
 	new_direction = new_direction.normalized()
 	last_direction = new_direction
 
+func update_health():
+	healthBar.value = health
+	healthBar.visible = health < 100
+
+func die():
+	if health <= 0 and not isDead:
+		health = 0
+		isDead = true
+		animated_sprite.play("die")
+		animated_sprite.animation_finished
+		queue_free()
+		
 func _on_territoty_body_entered(body):
 	if body.is_in_group("Player"):
 		player = body
@@ -90,6 +111,9 @@ func _on_hitbox_enemy_1_body_entered(body):
 		print("Swooping")
 		#animated_sprite.flip_v = position.y > body.position.y
 		animated_sprite.flip_h = position.x > body.position.x
+		swoop_speed = 0
+		health -= 20
+		body.health -= 20
 
 
 func _on_hitbox_enemy_1_body_exited(body):
@@ -98,3 +122,4 @@ func _on_hitbox_enemy_1_body_exited(body):
 		print("Player exited the hitbox!")
 		animated_sprite.flip_v = false
 		animated_sprite.flip_h = false
+		swoop_speed = 75
